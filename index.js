f9922af50b4c64b2ae73424b5a1255c18bec4ee1
mercy wanjiku maruga <mercy.maruga@student.moringaschool.com>
2022-08-17 16:10:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+
+const mondayWork = function(job = 'go to the office'){
+    return (`This Monday, I will ${job}.`);
+}
+
+function wrapAdjective(adjective = "*"){
+    return function(wrap = ("special")){
+        return(`You are ${adjective}${wrap}${adjective}!`);
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
